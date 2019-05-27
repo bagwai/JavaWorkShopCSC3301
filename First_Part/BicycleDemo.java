28b69d375d73432c5cabe3ef535b9ae27422515d
bagwai <mubarakdaha8@gmail.com>
2019-05-27 01:42:34 +0100

    Added the lectures slides including how to swing tutorial

--- First_Part/BicycleDemo.java
+++ First_Part/BicycleDemo.java
@@ -30,9 +30,9 @@ class Bicycle {
   }
 }
 
-class BicycleDemo{
+class BicycleDemo {
   public static void main(String[] args) {
-    //creating 2 diffrent Bicycle objects
+    // creating 2 diffrent Bicycle objects
 
     Bicycle bike1 = new Bicycle();
     Bicycle bike2 = new Bicycle();
@@ -44,4 +44,4 @@ class BicycleDemo{
     bike2.speedUp(15);
     bike2.printStates();
   }
-}
+}
